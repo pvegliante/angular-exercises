b6e63bec07c9b22f07ebcd36a018333b0b891b3c
pvegliante@fvi.edu <pvegliante@fvi.edu>
2017-03-30 21:30:55 -0400

    done

--- app/BlogPostCtrl.js
+++ app/BlogPostCtrl.js
@@ -1,11 +1,26 @@
 (function ()
 {
     'use strict';
-    function BlogPostCtrl()
+    function BlogPostCtrl($scope)
     {
-
+      $scope.blogs = [{
+        id: 1,
+        Author: 'Jack',
+        Title: 'Up Jills hill'
+      },
+      {
+        id: 2,
+        Author: 'Jill',
+        Title: 'Jacks Bean Stalk'
+      },
+      {
+        id: 3,
+        Author: 'Wolf',
+        Title: 'This Blows'
+      }
+    ]; console.log('alisdhf');
     }
 
     var module = angular.module("exerciseApp", []);
-    module.controller('BlogPostCtrl', [BlogPostCtrl]);
+    module.controller('BlogPostCtrl', ['$scope', BlogPostCtrl]);
 })();
